17c979504e265ca1224064a280264bc982d2580d
powMo1-xiscer-gutrop <zhaniyas788@gmail.com>
2024-06-01 14:44:37 +0500

    adding files

---
+++ classovyi komponent/src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Welcome extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Classovyi komponent</h1>
+        <h2>Еще один классовый компонент</h2>
+      </div>
+    );
+  }
+}
+
+export default Welcome;
